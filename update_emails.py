d810d362d862ace0a4a239923908c5b370652ad5
Connor Christian <cjc13e@intern-01.rcc.fsu.edu>
2017-07-13 13:02:49 -0400

    Removed the requests and re modules
    Splinter and Requests acheived much of the same functionality however splinter
    had more built in features. By fully utilizing Splinter I was able to remove
    the requests and re modules.
    
    Not only was I able to more accurately scrape data without relying on re, the
    code was less redundant.
    
    There are now more logs printed out and comments added.
    
    Requirements.txt has been updated along with the test files ___Emails.txt

--- update_emails.py
+++ update_emails.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 from splinter import Browser
 from dotenv import load_dotenv, find_dotenv
-import requests, time, os, sys, re
+import time, os, sys
 
+#collect login data collected from .env
 load_dotenv(find_dotenv())
-
-#login data
 PSWD = os.getenv('PASSWORD')
 URI = os.getenv('LISTSERV_URI')
 
@@ -16,30 +15,42 @@ assert URI, "No uri in .env\nAborting"
 #suppress stderr because of ignorable selenium or requests warnings
 sys.stderr = open(os.devnull, 'w')
 
-'''
-lists to be filled with scraped data
-format: [("lastName","firstName","<email>"), (...)]
-'''
-web_emails = []
-db_emails = []
+#lists to be filled with scraped data
+web_emails = [] #format: ["email"]
+db_emails = [] #format: ["email"]
+db_content = {} #format: key="email" value="lname fname"
 
+''' These strings store newline separated emails to be added to or removed
+    from the webserv depending on the contents of the database and webserv.
 '''
-the following two data types contain email info based on
-the relationship between the above two lists
-string of strings: "last first <email>\n..."
-'''
-add_emails = "" 
-rm_emails = ""
+add_emails = "" #"lname fname <email>\n..."
+rm_emails = "" #"email\n..."
 
-def setEmails(f):
-    # temporary: will be replaced when acutally getting data from a database and
-    # not a textfile   global db_emails
+#TODO: Replace with real database commands
+def setDB_Content(f):
+    ''' Collects the emails from the "database" to be used later
+    '''
+    global db_content
+    global db_emails
+
     with open(f, 'r') as file:
         for line in file:
             lname, fname, email = line.split()
-            db_emails.append((lname, fname, email[1:-1]))
+            db_content[email] = lname + ' ' + fname
+    db_emails = db_content.keys()
 
+#TODO: Replace with real database commands
+def getDB_Content(email):
+    ''' Takes an email known to be in the database and returns a string
+        with the associated first and last name
+    '''
+    global db_content
 
+    try:
+        return db_content[email]
+    except:
+        return ' '
+        
 def update():
     ''' compares email lists and appends data to appropriate add/rm_email data structs.
         * if (email in webserv but not datatbase) remove;
@@ -49,16 +60,18 @@ def update():
     global db_emails
     global rm_emails
     global add_emails
+    
+    log("Comparing emails found on webserv with emails in database")
 
     #compares every email from the webserv to those found in the database
     for web_data in web_emails:
         if web_data not in db_emails: #if true, then that email must be removed
-            rm_emails += web_data[0] + ' ' + web_data[1] + " <" + web_data[2] + ">\n"
+            rm_emails += web_data + '\n'
     
     #compares every email from the database to those found in the webserv
     for db_data in db_emails:
         if db_data not in web_emails: #if true, then that email must be added
-            add_emails += db_data[0] + ' ' + db_data[1] + " <" + db_data[2] + ">\n"
+            add_emails += getDB_Content(db_data)  + ' <' + db_data + '>\n'
 
     removeEmails(rm_emails)
 
@@ -68,32 +81,31 @@ def log(message):
     ''' logs in the format of: "timestamp removed/added email"
         time stamp in the format: "YYYY-mm-dd hh:mm:ss"
     '''
-    #log += time.strftime("%Y-%m-%d %H:%M:%S") + ' ' + status + ' ' + email + '\n'
     print(time.strftime("%Y-%m-%d %H:%M:%S") + ' ' + message)
 
 def removeEmails(e, l="enable"):  #remove all emails on the site
+    ''' e: string of emails newline separated
+        l: is a flag whether or not to log the action, the default is to log
+    '''
     browser.visit(URI + '/members/remove')
     browser.fill('unsubscribees', e)
     browser.find_by_name('setmemberopts_btn').click()
-    browser.back()
-    browser.reload()
 
     if l == "enable":
         for email in e.split('\n')[:-1]:
-            email = email.split('<')[1][:-1]
             log("removed " + email)
 
 def addEmails(e, l="enable"):  #remove all emails on the site
+    ''' e: string of emails newline separated
+        l: is a flag whether or not to log the action, the default is to log
+    '''
     browser.visit(URI + '/members/add')
     browser.fill('subscribees', e)
     browser.find_by_name('setmemberopts_btn').click()
-    browser.back()
-    browser.reload()
 
     if l == "enable":
         for email in e.split('\n')[:-1]:
-            email = email.split('<')[1][:-1]
-            log("added " + email)
+            log("added " + email.split('<')[1][:-1])
         
 #go to mailserv site and login
 browser = Browser('phantomjs') #remove 'phantomjs' to use default firefox
@@ -101,53 +113,29 @@ browser.visit(URI)
 browser.fill('adminpw',PSWD)
 browser.find_by_name('admlogin').click()
 
-#login with requests session so I can pull html to parse
-session = requests.session()
-login_data = dict(adminpw=PSWD)
-r = session.post(URI, data=login_data)
-
 #navigate to membership management
-browser.click_link_by_href('../admin/hpc-test/members')
+browser.visit(URI + '/members/list')
 
-#get membership list html and check if empty or not
-req = session.get(URI + '/members/list')
-info = re.search(r'<em>0 members total</em>',req.text)
+setDB_Content("mixEmails.txt")
+log("Database data is collected")
 
-temp = ''
-while info is None: #emails found
-    #addresses grabs every email from the html page
-    addresses = map(str,re.findall(r'[a-zA-Z0-9!\#\$\%\&\'\*\+\-\/\=\?\^\_\`\{\|\}\~\.]+?@[a-zA-Z0-9\-\.]+?</a>',req.text))
-    #names grabs every name from the html page
-    names = map(str,re.findall(r'value="\w* \w*"',req.text))
-    #data associates the two above such that each name is linked with an email
-    data = zip(names, addresses)  
+temp=''
+while browser.is_text_not_present('0 members total'): #emails found
+    log("Grabbing data from webserv...")
+    links = browser.find_link_by_partial_href('--at--')
+
+    for a in links:
+        web_emails.append(a.value)
+        temp += getDB_Content(a.value) + ' <' + a.value + '>\n'
     
-    for d in data:
-        lname, fname = d[0][7:-1].split()
-        email = d[1][:-4]
-        web_emails.append((lname, fname, email))
-
-    for t in web_emails:
-        temp += t[0] + ' ' + t[1] + " <" + t[2] + ">\n"
     removeEmails(temp, "disable")
-
-    req = session.get(URI + '/members/list')
-    info = re.search(r'<em>0 members total</em>',req.text)
+    browser.visit(URI + '/members/list')
 #else no emails
 addEmails(temp, "disable")
-
-#At this point, webserv data is collected: names and emails
-
-#Collect all the emails of current users somehow from the servers
-#add emails from RCC database
-#temp vvv
-setEmails('mixEmails.txt')
-#temp ^^^
-
-#At this point, db data is collected: names and emails
+log("Webserv data is collected")
 
 update()
-log("Webserv and Database are up to date.\n")
+log("Webserv and Database are synced")
 
 #logout before exiting
 browser.visit(URI + '/logout')
